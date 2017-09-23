52343cf1ff5b39854d501118060d5c4c6856f0c4
Mads Hartmann <mads379@gmail.com>
2017-09-23 10:38:18 +0200

    Tiny example of an electron app that talks to Docker

---
+++ electron-talks-to-docker/README.md
@@ -0,0 +1,6 @@
+# Electron 💬 Docker
+
+A tiny electron app that talks to Docker over a unix socket.
+
+    npm install
+    npm run start
---
+++ electron-talks-to-docker/docker.js
@@ -0,0 +1,53 @@
+const http = require('http');
+
+function request(path, method) {
+    var method = method ? method : 'GET';
+    const req = {
+        method: method,
+        protocol: 'http:',
+        socketPath: '/var/run/docker.sock',
+        path: path
+    }
+    return new Promise((resolve, reject) => {
+        http.get(req, (res) => {
+            res.setEncoding('utf8');
+            let rawData = '';
+            res.on('data', (chunk) => rawData += chunk);
+            res.on('end', () => {
+                try {
+                    if (rawData !== '') {
+                        let parsedData = JSON.parse(rawData);
+                        resolve(parsedData);
+                    } else {
+                        resolve()
+                    }
+                } catch (e) {
+                    reject(e.message);
+                }
+            });
+        })
+    });
+}
+
+function containers() {
+    return request('/containers/json?all=1')
+        .then((response) => {
+            return response.map(container => {
+                const name = container.Names.join(" ").replace("/", "");
+                return {
+                    name: name,
+                    container_id: container.Id,
+                    state: container.State,
+                    ports: container.Ports.map(port => ({
+                        publicPort: port.PublicPort,
+                        privatePort: port.PrivatePort
+                    }))
+                }
+            });
+        })
+}
+
+module.exports = {
+    containers,
+    request
+};
