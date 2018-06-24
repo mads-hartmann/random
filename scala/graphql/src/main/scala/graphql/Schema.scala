19e0b0486b1a828efcfa1aaaf7c9ab98e207dd04
Mads Hartmann <mads379@gmail.com>
2018-06-24 20:31:49 +0200

    Fixed the problem - It seems

--- scala/graphql/src/main/scala/graphql/Schema.scala
+++ scala/graphql/src/main/scala/graphql/Schema.scala
@@ -6,10 +6,8 @@ object Schema {
     *
     * Implementation note:
     *
-    * Trying to use a GADT here to enforce that types are nullable by default
+    * A GADT is used here to enforce that types are nullable by default
     * and should be marked non-nullable in the Schema using [[NonNull]].
-    *
-    * I haven't been successful - see [[toJSON]].
     */
   sealed trait Type[S]
 
@@ -96,57 +94,63 @@ object Schema {
     }
   }
 
-  //
-  // What I tried to do:
-  //
-  // Use a GADT to model a Type such that each of the constructors _should_ be
-  // able to give the Scala compiler some more knowledge of the type S; see
-  // comments in each of the pattern matches below for details.
-  //
-  // What I got:
-  //
-  // Code that compiles but breaks at runtime with an exception:
-  //
-  //     java.lang.Integer cannot be cast to scala.runtime.Nothing$
-  //
-  // See SchemaSpecification.scala for concrete example.
-  //
-  // Help:
-  //
-  // How can I get this to either:
-  //
-  // - Not break during runtime or
-  // - Not compile in a meaningful way.
-  //
+  /**
+    * Given a source value of type [[S]] and a [[Type]] with source
+    * type [[S]] traverse the graph to produce a [[Json]] value.
+    *
+    * Implementation note:
+    *
+    * As [[Type]] is a GADT the Scala compiler can use the constructors to
+    * infer information about the concrete type [[S]]. See implementation
+    * for comments.
+    *
+    * I had to split this into two methods, [[toJSON]] and [[toJSONOpt]] as
+    * the Scala compiler would otherwise infer [[S]] to be Any which resulted
+    * in runtime exceptions in some cases.
+    *   See commit 60d3c32a3580f4f460a58831bbedbf96a2388f29 for the original
+    * implementation which had problems.
+    */
   def toJSON[S](source: S, tpe: Type[S]): Json = tpe match {
 
-    case Scalar(_, serialize) =>
-      // Here the Scala compiler should know that S really is an Option[_] as
-      // Scalar[S] is a Type[Option[S]]
-      source match {
-        case None    => JNull
-        case Some(s) => serialize(s)
-      }
+    // Here the Scala compiler knows that S really is an Option[_] as
+    // Scalar[S] and Object[S] both have type Type[Option[S]]
+    case Scalar(_, serialize) => toJSONOpt(source, tpe)
+    case Object(_, fields)    => toJSONOpt(source, tpe)
 
-    case Object(_, fields) =>
-      // Here the Scala compiler should know that S really is an Option[_] as
-      // Object[S] is a Type[Option[S]]
-      source match {
-        case Some(s) => {
-          val members = fields.map { field =>
-            val json = toJSON(field.resolve(s), field.output)
-            (field.name, json)
-          }
-          JObject(members)
-        }
-        case None => JNull
-      }
-
-    case NonNull(inner) =>
-      // Here the Scala compiler should know that S is just a simple type (not an Option)
-      // and that inner has type Type[Option[S]].
-      toJSON(Some(source), inner)
+    // Here the Scala compiler should know that S is just a simple type (not an
+    // Option) and that inner has type Type[Option[S]].
+    case NonNull(inner) => toJSONOpt(Some(source), inner)
 
   }
+
+  private def toJSONOpt[S](source: Option[S], tpe: Type[Option[S]]): Json =
+    tpe match {
+      case Scalar(_, serialize) =>
+        source match {
+          case None    => JNull
+          case Some(s) => serialize(s)
+        }
+      case Object(_, fields) =>
+        source match {
+          case Some(s) => {
+            val members = fields.map { field =>
+              val json = toJSON(field.resolve(s), field.output)
+              (field.name, json)
+            }
+            JObject(members)
+          }
+          case None => JNull
+        }
+      // I really wish the compiler could look at the GADT and see that the
+      // only two relevant cases for Type[Option[_]] is Scalar and Object.
+      case NonNull(_) =>
+        throw new Exception(
+          """
+          |So this is a bit embarrassing.
+          |The compiler should have stopped you from constructing a schema that
+          |contains a non-nullable optional value but somehow you manged.
+        """.stripMargin
+        )
+    }
 
 }
--- scala/graphql/src/test/scala/graphql/SchemaSpecification.scala
+++ scala/graphql/src/test/scala/graphql/SchemaSpecification.scala
@@ -1,5 +1,6 @@
 package graphql
 
+import graphql.Schema.Type
 import org.scalatest.{FreeSpec, Matchers}
 
 class SchemaSpecification extends FreeSpec with Matchers {
@@ -17,12 +18,41 @@ class SchemaSpecification extends FreeSpec with Matchers {
   }
 
   "Schema.toJSON" - {
-    "Scalars" - {
+    "Scalars" in {
       // TODO: This tests fails with java.lang.Integer cannot be cast to scala.runtime.Nothing$
       // TODO: If you remove the Some wrapping you get a very unhelpful error message - can we fix that?
       assert(Schema.toJSON(Some(42), Schema.Scalar.int) == JInt(42))
       assert(
         Schema.toJSON(Some("hello"), Schema.Scalar.string) == JString("hello"))
+    }
+
+    "Object" in {
+      case class User(id: Int, name: String)
+      val userT = Schema.Object(
+        name = "User",
+        fields = List(
+          Schema.Field("id", Schema.Scalar.int, (user: User) => Some(user.id)),
+          Schema.Field("name",
+                       Schema.Scalar.string,
+                       (user: User) => Some(user.name))
+        )
+      )
+      val json = Schema.toJSON(Some(User(42, "Mads")), userT)
+      val expected = JObject(
+        List(
+          ("id", JInt(42)),
+          ("name", JString("Mads"))
+        ))
+
+      assert(json == expected)
+
+    }
+
+    "NonNull" in {
+      assert(Schema.toJSON(42, Schema.NonNull(Schema.Scalar.int)) == JInt(42))
+      assert(
+        Schema.toJSON("hello", Schema.NonNull(Schema.Scalar.string)) == JString(
+          "hello"))
     }
   }
 
