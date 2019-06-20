8ea1f13ff644af0211113c1fa4a2c8e08d945980
Mads Hartmann <mads379@gmail.com>
2019-06-20 11:27:04 +0200

    Works using automatic http plugin

--- observability/open-census/src/service-a.js
+++ observability/open-census/src/service-a.js
@@ -8,26 +8,32 @@ const {TraceContextFormat} = require('@opencensus/propagation-tracecontext');
 const middleware = require('./middleware');
 
 const port = process.env.PORT || 8080
-const tracer = tracing.start({samplingRate: 1}).tracer;
 
-tracer.registerSpanEventListener(new ZipkinTraceExporter({
-  url: 'http://localhost:9411/api/v2/spans',
-  serviceName: 'service-a',
-  logLevel: 1 // show errors, if any
-}));
-
-
-tracing.start({propagation: new TraceContextFormat()});
+tracing.start({
+  exporter: new ZipkinTraceExporter({
+    url: 'http://localhost:9411/api/v2/spans',
+    serviceName: 'service-a',
+    // logLevel: 1 // show errors, if any
+  }),
+  propagation: new TraceContextFormat(),
+  samplingRate: 1,
+});
 
 const app = express();
 
-app.get('/', middleware.tracing(tracer), async (request, response) => {
+app.get('/', async (request, response) => {
+  console.log(`headers: ${JSON.stringify(request.headers)}`);
   const reply = await axios.get('http://localhost:8081/sleep/2000')
   // console.log(reply)
   response.status(reply.status);
   response.send(reply.data);
 });
 
-const listener = app.listen(port, () => {
-  console.log(`Your app is listening on http://localhost:${listener.address().port}`);
-});
+// const listener = app.listen(port, () => {
+//   console.log(`Your app is listening on http://localhost:${listener.address().port}`);
+// });
+
+const http = require('http');
+app.set('port', port);
+const server = http.createServer(app);
+server.listen(port);
--- observability/open-census/src/service-b.js
+++ observability/open-census/src/service-b.js
@@ -7,23 +7,30 @@ const {TraceContextFormat} = require('@opencensus/propagation-tracecontext');
 const middleware = require('./middleware');
 
 const port = process.env.PORT || 8081
-const tracer = tracing.start({samplingRate: 1}).tracer;
 
-tracer.registerSpanEventListener(new ZipkinTraceExporter({
-  url: 'http://localhost:9411/api/v2/spans',
-  serviceName: 'service-b',
-  logLevel: 1 // show errors, if any
-}));
-
-tracing.start({propagation: new TraceContextFormat()});
+tracing.start({
+  exporter: new ZipkinTraceExporter({
+    url: 'http://localhost:9411/api/v2/spans',
+    serviceName: 'service-b',
+    // logLevel: 1 // show errors, if any
+  }),
+  propagation: new TraceContextFormat(),
+  samplingRate: 1,
+});
 
 const app = express();
 
-app.get('/sleep/:timeout', middleware.tracing(tracer), (request, response) => {
+app.get('/sleep/:timeout', (request, response) => {
+  console.log(`headers: ${JSON.stringify(request.headers)}`);
   response.status(200);
   response.send("okay from service-b");
 });
 
-const listener = app.listen(port, () => {
-  console.log(`Your app is listening on http://localhost:${listener.address().port}`);
-});
+// const listener = app.listen(port, () => {
+//   console.log(`Your app is listening on http://localhost:${listener.address().port}`);
+// });
+
+const http = require('http');
+app.set('port', port);
+const server = http.createServer(app);
+server.listen(port);
